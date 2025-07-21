ef4df8879e3f57f2b39a0223f1b7ac1521d2ab69
Denizdius <26deniz53@gmail.com>
2025-07-21 16:06:41 +0300

    full path

--- prepare_depo.sh
+++ prepare_depo.sh
@@ -2,4 +2,4 @@
 apt -y install  gcc-11 g++-11
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 11
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 11
-cd ../split/profiling_injection && ./build.sh
+cd /data/lm-evaluation-harness/split/profiling_injection && ./build.sh
